e5fa65bb189b3c9ff2c9bb10e7640e9c48acc624
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-03-07 15:25:42 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -17,12 +17,25 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About(){
+  return(
+    <>
+   <div id="about">
+    <h2>About</h2>
+   </div>
+    
+    </>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      
+      <NavBar/>
+      <Home/>
+      <About/>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
